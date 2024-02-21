2ebeef338d09e06275d006864147caa7f512709a
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-21 15:53:11 -0300

    Utilizando rotas aninhadas

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,4 +1,4 @@
-import Banner from "componentes/Banner";
+
 import styles from './Inicio.module.css';
 
 import posts from 'json/posts.json';
@@ -6,8 +6,6 @@ import Post from "componentes/Post";
 
 export default function Inicio() {
     return (
-<main>
-        <Banner />
        <ul className={styles.posts}>
             {posts.map((post)=> (
                 <li key={post.id}> 
@@ -15,7 +13,5 @@ export default function Inicio() {
                 </li>
             ))}
        </ul>
-
-        </main>
     )
 }
