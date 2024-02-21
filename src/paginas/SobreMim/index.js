514cb89b6ad8b0699c165cbb307f2f3c7d9c4322
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-20 21:33:47 -0300

    Criando o roteador

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,5 @@
-export default function SobreMim(){
-    return(
-        <h1>Sobre Mim...</h1>
+export default function SobreMim() {
+    return (
+        <h1>Sobre mim...</h1>
     )
 }
--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,5 +1,5 @@
 export default function Inicio() {
-    return(
-        <h1>Olá Mundo!</h1>
+    return (
+        <h1>Olá mundo!</h1>
     )
 }
