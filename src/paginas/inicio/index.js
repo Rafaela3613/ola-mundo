57d8e43041e0c5d9d06f537823929b39d0ab27f2
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-21 11:08:29 -0300

    criando o jsconfig.json

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,5 +1,10 @@
+import Banner from "../../componentes/Banner";
+
 export default function Inicio() {
     return (
+<main>
+        <Banner></Banner>
         <h1>Olá mundo!</h1>
+        </main>
     )
 }
