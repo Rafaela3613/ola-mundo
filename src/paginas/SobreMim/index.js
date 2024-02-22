61b36d561e9189376229584d03d396f01d285f7b
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-21 22:17:35 -0300

    incluindo o texto sobre mim

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -11,33 +11,29 @@ export default function SobreMim() {
             titulo="Sobre mim"
         >
             <h3 className={styles.subtitulo}>
-                Olá, eu sou o Antônio!
+                Olá, eu sou a Rafaela!
             </h3>
 
             <img
                 src={fotoSobreMim}
-                alt="Foto do Antônio Evaldo sorrindo"
+                alt="Foto de Rafaela de oculos"
                 className={styles.fotoSobreMim}
             />
 
             <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+            Olá! Sou a Rafaela e estou muito empolgada em compartilhar com vocês a minha jornada de transformação da barbearia para o mundo do desenvolvimento de software, enquanto equilibro minha vida como esposa e mãe de dois meninos.
             </p>
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+            Recentemente, descobri uma paixão pela tecnologia e decidi buscar uma mudança de carreira para o desenvolvimento de software. Nos últimos meses, tenho me dedicado intensamente a cursos, bootcamps e projetos práticos, explorando linguagens como Javascript, Typescript, CSS, HTML e frameworks como o React, e o Angular.
             </p>
             <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
+            Esta jornada desafiadora tem sido gratificante e estimulante, permitindo-me desenvolver novas habilidades e aplicar minha criatividade de maneiras totalmente novas. Acredito firmemente que as habilidades da barbearia, como atenção aos detalhes e foco no cliente, são tão valiosas no desenvolvimento de software quanto são na cadeira de barbeira.
             </p>
             <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
+            Equilibrar minha vida pessoal como esposa e mãe é crucial enquanto me aventuro neste novo campo. 
             </p>
             <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
-
-            </p>
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
+            Sinto que este momento é como plantar as sementes de um futuro onde minha criatividade e habilidades em tecnologia se combinam para criar soluções verdadeiramente impactantes. Estou pronta para ver essas ideias florescerem e iluminarem o caminho à frente. 
             </p>
         </PostModelo>
     )
